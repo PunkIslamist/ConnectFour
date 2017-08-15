46c01402dc4866edd9895e08ec8695953b97d077
PunkIslamist <achondrias@gmail.com>
2017-08-15 19:48:46 +0200

    REDO EVERYTHING!

--- src/model/Board.cs
+++ src/model/Board.cs
@@ -1,41 +1,6 @@
 namespace ConnectFour
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-
   public class Board
   {
-    private const int rowCount = 6;
-    private const int columnCount = 7;
-
-    public Board()
-    {
-      this.Fields = Enumerable.Range(0, rowCount * columnCount)
-          .Select(it => 'O');
-    }
-
-    public Board(IEnumerable<char> fields)
-    {
-      this.Fields = fields;
-    }
-
-    public static char EmptyField { get => 'O'; }
-    public IEnumerable<char> Fields { get; }
-    public IEnumerable<IEnumerable<char>> Rows { get; }
-    public IEnumerable<IEnumerable<char>> Columns { get; }
-
-    public Board MakeMove(int column, char symbol)
-    {
-      var firstColumn = Enumerable.Range(0, rowCount - 1)
-        .Select(it => EmptyField)
-        .Append(symbol);
-      var remainingColumns = this.Columns.Skip(1);
-
-      var allFields = remainingColumns.Prepend(firstColumn)
-        .SelectMany(it => it);
-
-      return new Board(allFields);
-    }
   }
 }
--- src/test/BoardTest.cs
+++ src/test/BoardTest.cs
@@ -7,37 +7,12 @@ namespace ConnectFour.Test
 
   public class BoardTest
   {
-    private const char p1 = 'X';
-    private const char p2 = 'T';
-    private const int rowCount = 6;
-    private const int columnCount = 7;
-
     [Fact]
     public void Constructor_Default_ReturnsEmptyBoard()
     {
-      var board = this.CreateBoard();
-      var expected = Enumerable.Range(0, rowCount * columnCount)
-        .Select(it => Board.EmptyField);
+      var board = new Board();
 
-      var actual = board.Fields;
-
-      Assert.Equal(expected, actual);
-    }
-
-    [Fact]
-    public void MakeMove_XInFirstColumn_FieldBottomLeftIsX()
-    {
-      var board = this.CreateBoard();
-
-      var actual = board.MakeMove(column: 0, symbol: p1);
-
-      Assert.Equal(actual.Columns.First().Last(), p1);
-      Assert.Equal(columnCount * rowCount - 1, actual.Fields.Count());
-    }
-
-    private Board CreateBoard()
-    {
-      return new Board();
+      Assert.True(board.Count() == 42);
     }
   }
 }
