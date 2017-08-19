2f68a0bc5ab4478f5104cf42d0821b4c4a1cc42b
PunkIslamist <achondrias@gmail.com>
2017-08-19 15:32:48 +0200

    Moved getter logic for Board.Columns into a method.

--- src/model/Board.cs
+++ src/model/Board.cs
@@ -6,38 +6,37 @@ namespace ConnectFour.Model
 
   public class Board
   {
-    private const int rowCount = 6;
-    private const int columnCount = 7;
-    private char[,] fields = new char[columnCount, rowCount];
+    private const int rows = 6;
+    private const int columns = 7;
+    private char[,] fields = new char[columns, rows];
 
     public Board()
     {
-      for (int col = 0; col < columnCount; ++col)
+      for (int col = 0; col < columns; ++col)
       {
-        for (int row = 0; row < rowCount; ++row)
+        for (int row = 0; row < rows; ++row)
         {
           this.fields[col, row] = this.EmptyField;
         }
       }
     }
 
-    public IEnumerable<IEnumerable<char>> Columns
-    {
-      get
-      {
-        for (int col = 0; col < columnCount; ++col)
-        {
-          var column = new char[rowCount];
-
-          for (int row = 0; row < rowCount; ++row)
-          {
-            column[row] = this.fields[col, row];
-          }
-          yield return column;
-        }
-      }
-    }
+    public IEnumerable<IEnumerable<char>> Columns => this.GetColumns();
 
     public char EmptyField { get; } = '_';
+
+    private IEnumerable<IEnumerable<char>> GetColumns()
+    {
+      for (int col = 0; col < columns; ++col)
+      {
+        var column = new char[rows];
+
+        for (int row = 0; row < rows; ++row)
+        {
+          column[row] = this.fields[col, row];
+        }
+        yield return column;
+      }
+    }
   }
 }
