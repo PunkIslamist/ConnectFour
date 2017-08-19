71a5558724f685f34f1ef864d1c52bcd72c01b0a
PunkIslamist <achondrias@gmail.com>
2017-08-19 15:21:26 +0200

    Started creating the board class. Basic constructor exists.

--- src/model/Board.cs
+++ src/model/Board.cs
@@ -1,9 +1,43 @@
 namespace ConnectFour.Model
 {
   using System;
+  using System.Collections.Generic;
+  using System.Linq;
 
   public class Board
   {
+    private const int rowCount = 6;
+    private const int columnCount = 7;
+    private char[,] fields = new char[columnCount, rowCount];
 
+    public Board()
+    {
+      for (int col = 0; col < columnCount; ++col)
+      {
+        for (int row = 0; row < rowCount; ++row)
+        {
+          this.fields[col, row] = this.EmptyField;
+        }
+      }
+    }
+
+    public IEnumerable<IEnumerable<char>> Columns
+    {
+      get
+      {
+        for (int col = 0; col < columnCount; ++col)
+        {
+          var column = new char[rowCount];
+
+          for (int row = 0; row < rowCount; ++row)
+          {
+            column[row] = this.fields[col, row];
+          }
+          yield return column;
+        }
+      }
+    }
+
+    public char EmptyField { get; } = '_';
   }
 }
--- test/BoardTest.cs
+++ test/BoardTest.cs
@@ -1,14 +1,30 @@
 namespace ConnectFour.Test
 {
   using System;
+  using System.Linq;
+  using ConnectFour.Model;
   using Xunit;
 
-    public class BoardTest 
+  public class BoardTest
+  {
+    [Fact]
+    public void CreateBoard_DefaultConstructor_CountIs42()
     {
-        [Fact]
-        public void Test1()
-        {
+      var board = new Board();
 
-        }
+      var actual = board.Columns.SelectMany(it => it);
+
+      Assert.True(actual.Count() == 6 * 7);
     }
+
+    [Fact]
+    public void CreateBoard_DefaultConstructor_AllColumnsEmpty()
+    {
+      var board = new Board();
+
+      var actual = board.Columns.SelectMany(it => it);
+
+      Assert.True(actual.All(it => it == board.EmptyField));
+    }
+  }
 }
