df3b9bdd0056119f2b397dd5a6db1706d4256a10
PunkIslamist <achondrias@gmail.com>
2017-09-03 13:40:16 +0200

    Move is using Column instead of int now.

--- src/model/Move.cs
+++ src/model/Move.cs
@@ -2,13 +2,13 @@ namespace ConnectFour.Model
 {
   public class Move
   {
-    public Move(Player movingPlayer, int playedColumn)
+    public Move(Player movingPlayer, Column playedColumn)
     {
       this.MovingPlayer = movingPlayer;
       this.PlayedColumn = playedColumn;
     }
 
     public Player MovingPlayer { get; }
-    public int PlayedColumn { get; }
+    public Column PlayedColumn { get; }
   }
 }
